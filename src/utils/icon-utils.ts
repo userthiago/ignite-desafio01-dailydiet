b2d94e8ffdcc238da5ba6f5197c978ba91773fb5
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-02-28 16:25:48 -0300

    feat: Adicionando helpers para ícones.

---
+++ src/utils/icon-utils.ts
@@ -0,0 +1,8 @@
+import { IconColorsType } from "@components/Icon";
+import { AccomplishmentTypes } from "./types/accomplishment-types";
+
+export const iconColorMap: Record<AccomplishmentTypes, IconColorsType> = {
+  NEUTRAL: "black",
+  SUCCESS: "green",
+  FAILURE: "red",
+};
