830bab3f43de2e75f68dc71f804e297db0c3660a
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-02-28 16:24:58 -0300

    feat: Adicionando componente de ButtonIcon.

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,19 @@
+import { Icon, IconColorsType, IconType } from "@components/Icon";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+type ButtonIconProps = TouchableOpacityProps & {
+  name: IconType;
+  color?: IconColorsType;
+};
+
+export function ButtonIcon({
+  name,
+  color = "black",
+  ...rest
+}: ButtonIconProps) {
+  return (
+    <TouchableOpacity {...rest}>
+      <Icon name={name} color={color} size={24} />
+    </TouchableOpacity>
+  );
+}
