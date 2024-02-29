562a0a3f1019095610763e3394722a5888eb5d4e
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-02-29 17:09:48 -0300

    feat: Adicionando validação de formulário básica no registro de refeição.

--- src/screens/AddMeal/index.tsx
+++ src/screens/AddMeal/index.tsx
@@ -1,17 +1,36 @@
 import { LayoutContainer } from "@components/LayoutContainer";
-import { Dimensions, Text, TouchableOpacity } from "react-native";
+import { Alert, Dimensions, Text, TouchableOpacity } from "react-native";
 
 import { Header } from "@components/Header";
 import { ScreenContent } from "@components/ScreenContent";
 import { Input } from "@components/Input";
-import { DateTimeInputGroupContainer } from "./styles";
+import {
+  ActionAreaContainer,
+  GroupColumnContainer,
+  GroupRowContainer,
+  GroupTitle,
+} from "./styles";
 import { InputDatepicker } from "@components/InputDatepicker";
 import { useState } from "react";
+import { Button } from "@components/Button";
+import { InputRadio } from "@components/InputRadio";
+import { AccomplishmentTypes } from "@utils/types/accomplishment-types";
+import { useNavigation } from "@react-navigation/native";
 
 export function RegisterMeal() {
+  const [nameValue, setNameValue] = useState("");
+  const [descriptionValue, setDescriptionValue] = useState("");
   const [dateValue, setDateValue] = useState("");
   const [timeValue, setTimeValue] = useState("");
+  const [radioValue, setRadioValue] = useState<AccomplishmentTypes | "">("");
+  const navigation = useNavigation();
 
+  const handleChangeNameValue = (value: string) => {
+    setNameValue(value);
+  };
+  const handleChangeDescriptionValue = (value: string) => {
+    setDescriptionValue(value);
+  };
   const handleChangeDateValue = (value: string) => {
     setDateValue(value);
   };
@@ -20,13 +39,40 @@ export function RegisterMeal() {
     setTimeValue(value);
   };
 
+  const handleChangeRadioValue = (value: AccomplishmentTypes) => {
+    setRadioValue(value);
+  };
+
+  const handleRegisterMeal = () => {
+    if (
+      nameValue.trim().length === 0 ||
+      descriptionValue.trim().length === 0 ||
+      dateValue.length === 0 ||
+      timeValue.length === 0 ||
+      radioValue.length === 0
+    ) {
+      Alert.alert("Nova refeição", "É necessário preencher todos os campos.");
+    } else {
+      navigation.navigate("home");
+    }
+  };
+
   return (
     <LayoutContainer variant={"NEUTRAL"}>
       <Header title="Nova refeição" />
       <ScreenContent style={{ gap: 20 }}>
-        <Input title="Nome" />
-        <Input title="Descrição" variant="textarea" />
-        <DateTimeInputGroupContainer>
+        <Input
+          title="Nome"
+          value={nameValue}
+          onChangeText={handleChangeNameValue}
+        />
+        <Input
+          title="Descrição"
+          variant="textarea"
+          value={descriptionValue}
+          onChangeText={handleChangeDescriptionValue}
+        />
+        <GroupRowContainer>
           <InputDatepicker
             title="Data"
             style={{ width: Dimensions.get("window").width / 2 - 34 }}
@@ -40,7 +86,29 @@ export function RegisterMeal() {
             mode="time"
             handleOnChange={handleChangeTimeValue}
           />
-        </DateTimeInputGroupContainer>
+        </GroupRowContainer>
+        <GroupColumnContainer>
+          <GroupTitle>Está dentro da dieta</GroupTitle>
+          <GroupRowContainer>
+            <InputRadio
+              variant="SUCCESS"
+              isSelected={radioValue === "SUCCESS"}
+              title="Sim"
+              style={{ width: Dimensions.get("window").width / 2 - 34 }}
+              onPress={() => handleChangeRadioValue("SUCCESS")}
+            />
+            <InputRadio
+              variant="FAILURE"
+              isSelected={radioValue === "FAILURE"}
+              title="Não"
+              style={{ width: Dimensions.get("window").width / 2 - 34 }}
+              onPress={() => handleChangeRadioValue("FAILURE")}
+            />
+          </GroupRowContainer>
+        </GroupColumnContainer>
+        <ActionAreaContainer>
+          <Button label="Cadastrar refeição" onPress={handleRegisterMeal} />
+        </ActionAreaContainer>
       </ScreenContent>
     </LayoutContainer>
   );
--- src/screens/AddMeal/styles.ts
+++ src/screens/AddMeal/styles.ts
@@ -1,8 +1,26 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
-export const DateTimeInputGroupContainer = styled.View`
+export const GroupRowContainer = styled.View`
   flex-direction: row;
   justify-content: space-between;
 
   width: 100%;
+`;
+
+export const GroupColumnContainer = styled.View`
+  width: 100%;
+  gap: 8px;
+`;
+
+export const GroupTitle = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
+`;
+
+export const ActionAreaContainer = styled.View`
+  flex: 1;
+  justify-content: flex-end;
 `;
