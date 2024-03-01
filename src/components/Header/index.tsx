7edd4b2afcd7f3c1f7b05f08beb8dbb59053f0cb
Thiago Santos <thiagosantosprofissional@gmail.com>
2024-03-01 19:45:10 -0300

    fix: Corrigindo botão de retorno do header.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,26 @@
 import { ButtonIcon } from "@components/ButtonIcon";
 import { HeaderContainer, Title } from "./styles";
-import { useNavigation, NavigationProp } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 
 type HeaderProps = {
   title: string;
+  backAction?: "home" | "previous-screen";
 };
 
-export function Header({ title }: HeaderProps) {
+export function Header({ title, backAction = "previous-screen" }: HeaderProps) {
   const navigation = useNavigation();
 
-  const handleGoBackHome = () => {
-    navigation.navigate("home");
+  const handleGoBack = () => {
+    return backAction === "home"
+      ? navigation.navigate("home")
+      : navigation.goBack();
   };
 
   return (
     <HeaderContainer>
       <ButtonIcon
         name="arrowLeft"
-        onPress={handleGoBackHome}
+        onPress={handleGoBack}
         style={{ position: "absolute", left: 24 }}
       ></ButtonIcon>
       <Title>{title}</Title>
